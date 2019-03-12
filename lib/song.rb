e3882c05f8ce02ec9d0cf984286c670aac579e80
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-12 16:57:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,13 +33,9 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(name = "Blank Space")
+  def self.find_or_create_by_name(name)
 
-    if self.find_by_name(name)
-      name
-    else
-      self.create_by_name(name)
-    end
+    self.find_by_name(name) || self.create_by_name(name)
 
   end
 
