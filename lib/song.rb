24540d62f73bc3511547e344f5642fc6e35305ba
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-12 16:15:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,4 +14,28 @@ class Song
     song = @@all << self.new
   end
 
+  def new_by_name
+
+  end
+
+  def create_by_name
+
+  end
+
+  def find_by_name
+
+  end
+
+  def find_or_create_by_name
+
+  end
+
+  def alphabetical
+
+  end
+
+  def new_from_filename
+
+  end
+
 end
