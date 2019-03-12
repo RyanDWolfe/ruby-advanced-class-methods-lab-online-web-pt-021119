c52371565cb6f6313889c417eb1bf90d2c924828
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-12 16:40:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,11 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
-  def find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all ||= song
+    song
 
   end
 
